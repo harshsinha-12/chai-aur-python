d7c0290f1a206ca56492e9d1226d09cecd6fb138
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-14 18:22:13 +0530

    sol 7

--- 02_conditionals/07_solution.py
+++ 02_conditionals/07_solution.py
@@ -1,5 +1,5 @@
-coffee_size = input("What size coffee would you like? (Small, Medium, Large): ").capitalize()
-extra_shot = input("Would you like an extra shot? (Y/N): ").upper()
+coffee_size : str = input("What size coffee would you like? (Small, Medium, Large): ").capitalize()
+extra_shot : str = input("Would you like an extra shot? (Y/N): ").upper()
 
 if extra_shot == 'Y':
     coffee = coffee_size + ' Coffee' + ' with an extra shot'
