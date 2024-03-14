86e079a9e1a80e61439a8055ad33fe960d490b09
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-14 14:15:33 +0530

    sol 4

---
+++ 02_conditionals/04_solution.py
@@ -0,0 +1,8 @@
+colour : str = input('Enter the colour of the fruit: ')
+if colour == 'green':
+    print('It is unripe')
+elif colour == 'yellow':
+    print('It is ripe')
+else:
+    print('It is overripe')
+
