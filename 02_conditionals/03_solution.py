e302abd9c9975939cd3ad08ed6366b38633a83c0
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-14 14:02:50 +0530

    sol 3

---
+++ 02_conditionals/03_solution.py
@@ -0,0 +1,14 @@
+s : int = int(input("Enter the marks:"))
+
+if s >= 90:
+    print("A")
+elif s >= 80:
+    print("B")
+elif s >= 70:
+    print("C")
+elif s >= 60:
+    print("D")
+else:
+    print("F")
+
+    
