9f3c52374613b46c588666f2fc962733fd1cecc0
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-16 00:33:31 +0530

    sol 7

--- 03_loops/07_solution.py
+++ 03_loops/07_solution.py
@@ -1,2 +1,8 @@
-n : int = int(input("Enter a number: "))
-
+while True:
+    n : int = int(input("Enter a number: "))
+    if n < 0 or n > 10:
+        print("Number is not in range [0, 10]! Exiting.")
+        break
+    else:
+        print("Number is in range [0, 10]. Try Again!")
+        continue
