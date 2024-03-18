491581cae1516ee7eef20678991466af4d672c07
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-18 13:32:01 +0530

    sol 1

--- 07_OPPs/01_solution.py
+++ 07_OPPs/01_solution.py
@@ -1,7 +1,28 @@
 class Car:
-    brand = None
-    model = None
+    def __init__(self, brand, model, color, year, price):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.year = year
+        self.price = price
 
-my_car = Car()
+my_car = Car("BMW", "X5", "Black", "2019", "1000000")
 
-print(my_car)
+print(my_car.brand)
+print(my_car.model)
+print(my_car.color)
+print(my_car.year)
+print(my_car.price)
+
+my_new_car = Car("Audi", "A6", "White", "2020", "1200000")
+
+print(my_new_car.brand)
+print(my_new_car.model)
+print(my_new_car.color)
+print(my_new_car.year)
+print(my_new_car.price)
+
+# init method is called automatically when a new object is created
+# self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class
+# __init__ method is used to initialize the object's state
+# __init__ method is called constructor method in Python
