274895e01793a3e430e67baf14fc3a2f9048b132
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-18 15:26:55 +0530

    sol 3

--- 07_OPPs/03_solution.py
+++ 07_OPPs/03_solution.py
@@ -1,2 +1,18 @@
-class ElectricCar:
-    def __init__()
+class Car:
+    def __init__(self, brand, model, color, year, price):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.year = year
+        self.price = price
+
+    def full_details(self):
+        return f"{self.brand} {self.model} {self.color} {self.year} {self.price}"
+    
+class ElectricCar(Car):
+    def __init__(self, brand, model, color, year, price, battery_size):
+        super().__init__(brand, model, color, year, price)
+        self.battery_size = battery_size
+
+my_electric_car = ElectricCar("Tesla", "Model S", "Red", "2021", "1500000", "100kWh")
+print(my_electric_car.full_details())
